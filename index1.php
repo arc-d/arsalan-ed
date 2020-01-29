d51e6aa09e88083a63fb97d9b8553efefab92ac5
arc-d <54093556+arc-d@users.noreply.github.com>
2020-01-29 15:03:55 +0330

    added some php codes to index1.php

---
+++ index1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <body>
+        <?php
+            echo "%HelloWorld%";
+			$x = 2;
+			$y = 5;
+			$z = $x+$y;
+			echo "<br>$z";
+			echo "<br>Ists x= $x + y= $y Bitch";
+
+        ?>
+    </body>
+</html>
